db767a35e7ec859a2de5b970dd266e913357a3cc
wmc2tokyo <wmcooper2@yahoo.com>
2018-11-24 22:09:55 +0900

    Started setting up search2.py for a CLI version and set it up to identify if the script is running on a pi-node or the macbook.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Lyrics Search
-
+_Old, need to update to include picluster_
 * A GUI tool. 
-* Performs string search recursively in `<cwd>/testlyrics/`. 
+* Performs pattern search concurrently using pi-cluster. 
 * Creates `<cwd>/results/` 
 * Creates text files in `<cwd>/results/`
 
@@ -10,7 +10,7 @@
 To search for a given pattern in all of the lyrics files.
 
 ### Operation
-
+_Old, need to update_
 * in the program's root dir, place any text file in `<root>/lyrics/`.
 * run `search.py`.
 * type in the exact pattern to search for.
@@ -18,7 +18,7 @@ To search for a given pattern in all of the lyrics files.
 * a list of files where the search pattern is found will appear in `<root>/results/`.
 
 ### To do
-* make a set of words from all the lryics from all the nodes
+* make a set of words from all the lyrics from all the nodes
 * copy that set to all the nodes
   * when searching for a pattern, check that the words exist in the set first.
 * make a regex that allows for searching a pattern of words within a list
@@ -27,3 +27,14 @@ To search for a given pattern in all of the lyrics files.
   * as big and tall as
 * standardized the dir for the data in all the files.
 * make a module to put in the nodes that uses multiprocessing.py
+
+### Process
+1. Divide all the files as evenly as possible among 4 dirs
+  * run `evenlydividework.py`
+2. Load the newly created dirs onto 4 USB's (because I have 4 pi-nodes)
+  * do this manually for now (basically only needs to be run once)
+3. Copy "evenlydividework.py" to the nodes
+4. Divide all the files on each node again into 4 smaller dirs
+  * run `evenlydividework.py`
+  * change the file paths where they will save to
+5. 
--- search2.py
+++ search2.py
@@ -2,11 +2,14 @@
 """A GUI tool for finding an exact pattern match."""
 
 #stand lib
+import os
 from pathlib import Path
 import re
 import time
 import tkinter as tk
 from tkinter import ttk
+
+#change targetdir to the data dir where the text files are kept
 
 class LyricsGui:
     def __init__(self):
@@ -25,7 +28,7 @@ class LyricsGui:
         self.userinput.grid(column=0, row=0, columnspan=4)
 
         self.searchbutton = ttk.Button(self.frame, 
-            text="Search", command=self._search)
+            text="Search", command=self.gui_search)
         self.searchbutton.grid(column=0, row=1, sticky=tk.W)
 
         self.quitbutton = ttk.Button(self.frame, 
@@ -50,7 +53,7 @@ class LyricsGui:
             savedir.mkdir()
         self.savedir = savedir
 
-    def _search(self):
+    def gui_search(self):
         """Searches for the user-requested pattern. Returns None."""
         self.songcount = 0
         self.matchcount = 0
@@ -77,12 +80,6 @@ class LyricsGui:
         resultsfile.close()
         print("PATTERN::", pattern, " :::  MATCHES::", str(self.matchcount))
 
-    def _threadedsearch(self):
-        """Sends string pattern to different module to perform a thread
-            based search. Returns None."""
-        pattern = self.userinput.get()
-        subprocess.run(["threadedsearch.py", pattern])
-
     def displaymatches(self):
         """Displays the file matches found in the gui. Returns None."""
         self.matchdisplay = ttk.Label(self.frame, 
@@ -94,4 +91,72 @@ class LyricsGui:
         self.totaldisplay = ttk.Label(self.frame, 
             text="Searched: "+str(self.songcount))
         self.totaldisplay.grid(column=3, row=1, sticky=tk.E)
-gui = LyricsGui()
+
+class LyricsCli:
+    def __init__(self):
+#        self.songcount = 0
+#        self.matchcount = 0
+        self.cwd = str(Path.cwd())
+        self.targetdir = Path("./testlyrics/")
+        self.makesavedir()
+
+    def makesavedir(self):
+        """Makes 'root/searchresults/'. Returns None."""
+        savedir = Path(self.cwd+"/results")
+        if not savedir.exists():
+            savedir.mkdir()
+        self.savedir = savedir
+
+    def cli_search(self):
+        """Searches for the user-requested pattern. Returns None."""
+        self.songcount = 0
+        self.matchcount = 0
+        pattern = self.userinput.get()
+
+        savefile = str(self.savedir)+"/"+pattern+".txt"
+        resultsfile = open(savefile, "a+")
+
+        #change this for the pi
+        #dont need to use glob if the queues are already setup
+        for file_ in Path(self.cwd).glob("**/*.txt"):
+            #change text file handler to context manager 'with'
+            text = open(file_, "r").read()
+            match = re.search(pattern, text)
+
+            if match != None:
+                resultsfile.write(str(file_)+"\n")
+                self.matchcount += 1
+                self.displaymatches()
+            self.songcount += 1
+            self.displaytotal()
+        resultsfile.close()
+        print("PATTERN::", pattern, " :::  MATCHES::", str(self.matchcount))
+
+    #print output to file
+    def displaymatches(self):
+        """Writes song matches found to a file. Returns None."""
+        pass
+
+    #print simplified output to terminal
+    def displaytotal(self):
+        """Prints song count match totals in the terminal. Returns None."""
+        pass
+
+def ismac():
+    """Checks if the machine is a mac. Returns Boolean."""
+    if os.uname().sysname == "Darwin": return True
+    else: return False
+
+def ispi():
+    """Checks if the machine is a pi node. Returns Boolean."""
+    if os.uname().sysname == "Linux": return True
+    else: return False
+
+if __name__ == "__main__":
+    if ismac():
+        gui = LyricsGui()
+    elif ispi():
+        cli = LyricsCli()
+        #needs to take a string pattern and display/log results
+    else:
+        print("Machine not recognized. Quitting program.")
