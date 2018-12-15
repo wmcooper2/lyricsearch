fba2ea1b720fb64cecfb3224323db1bebe0226f5
wmc2tokyo <wmcooper2@yahoo.com>
2018-12-15 15:11:13 +0900

    Playing with multiprocessing on the pi nodes. Need to fix the process exit?

--- oldcode/lyricsearch_pi.py
+++ oldcode/lyricsearch_pi.py
@@ -6,63 +6,66 @@ import os
 from pathlib import Path
 import re
 import subprocess as sp
-import time
+from time import time
 import tkinter as tk
 from tkinter import ttk
 
 #change targetdir to the data dir where the text files are kept
 
-class LyricsCli:
-    def __init__(self):
-#        self.songcount = 0
-#        self.matchcount = 0
-        self.cwd = str(Path.cwd())
-        self.targetdir = Path("./testlyrics/")
-        self.makesavedir()
+#        songcount = 0
+#        matchcount = 0
+cwd = str(Path.cwd())
+#targetdir = Path("./testlyrics/")
+#targetdir = Path("/mnt/usb/")
+targetdir = Path("/mnt/usb/data1/")
+savedir = Path(cwd+"/results")
 
-    def makesavedir(self):
-        """Makes 'root/searchresults/'. Returns None."""
-        savedir = Path(self.cwd+"/results")
-        if not savedir.exists():
-            savedir.mkdir()
-        self.savedir = savedir
+def makesavedir():
+    """Makes 'root/searchresults/'. Returns None."""
+    if not savedir.exists():
+        savedir.mkdir()
 
-    def cli_search(self):
-        """Searches for the user-requested pattern. Returns None."""
-        self.songcount = 0
-        self.matchcount = 0
-        pattern = self.userinput.get()
+def makeresultsfile(savefile):
+    """Makes a results file. Returns None."""
+    Path(savefile).touch()
+    os.chmod(savefile, 0o777)
 
-        savefile = str(self.savedir)+"/"+pattern+".txt"
-        resultsfile = open(savefile, "a+")
+def cli_search():
+    """Searches for the user-requested pattern. Returns None."""
+    songcount = 0
+    matchcount = 0
+    pattern = input("Enter a search pattern: ") 
+
+    savefile = str(savedir)+"/"+pattern+".txt"
+    makeresultsfile(savefile)
+    resultsfile = open(savefile, "a+")
 
 # use thread to send to nodes, use subprocess while inside the node
-
-        #change this for the pi
-        #dont need to use glob if the queues are already setup
-        for file_ in Path(self.cwd).glob("**/*.txt"):
-            #change text file handler to context manager 'with'
-            text = open(file_, "r").read()
+    for file_ in Path(targetdir).glob("**/*.txt"):
+        match = None
+        with open(str(file_), "r") as f:
+            text = f.read()
+            print("Searching...", file_)
             match = re.search(pattern, text)
 
-            if match != None:
-                resultsfile.write(str(file_)+"\n")
-                self.matchcount += 1
-                self.displaymatches()
-            self.songcount += 1
-            self.displaytotal()
-        resultsfile.close()
-        print("PATTERN::", pattern, " :::  MATCHES::", str(self.matchcount))
+        if match != None:
+            resultsfile.write(str(file_)+"\n")
+            matchcount += 1
+            displaymatches()
+        songcount += 1
+        displaytotal()
+    resultsfile.close()
+    print("PATTERN::", pattern, " :::  MATCHES::", str(matchcount))
 
-    #print output to file
-    def displaymatches(self):
-        """Writes song matches found to a file. Returns None."""
-        pass
+#print output to file
+def displaymatches():
+    """Writes song matches found to a file. Returns None."""
+    pass
 
-    #print simplified output to terminal
-    def displaytotal(self):
-        """Prints song count match totals in the terminal. Returns None."""
-        pass
+#print simplified output to terminal
+def displaytotal():
+    """Prints song count match totals in the terminal. Returns None."""
+    pass
 
 def ismac():
     """Checks if the machine is a mac. Returns Boolean."""
@@ -78,7 +81,12 @@ if __name__ == "__main__":
     if ismac():
         gui = LyricsGui()
     elif ispi():
-        cli = LyricsCli()
+#        cli = LyricsCli()
+        makesavedir()
+#        makeresultsfile()
+        cli_search()
         #needs to take a string pattern and display/log results
+        #load the data (dirs 1-4) paths into queues using all 4 cores. 
+        #time it
     else:
         print("Machine not recognized. Quitting program.")
